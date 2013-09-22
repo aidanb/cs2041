af4578a7d9f15db3b5e7300d3641205ab8525e04
Aidan Barrington <aidanb@gmail.com>
2013-09-22 13:21:21 +1000

    Finished first question.

---
+++ lab8/courses.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+# prints the course codes with a given prefix 
+#	of all UNSW courses with lectures on the Kensington Campus this session
+# Aidan Barrington UNSW COMP2041 2013 S2
+
+use strict;
+use warnings;
+
+my $year = 2013;
+my $url = "http://www.timetable.unsw.edu.au/current/";
+
+die "Usage: $0 Four-character UNSW course code"
+	if ((!@ARGV) or ($#ARGV > 0) or ($ARGV[0] !~ /^[a-z]{4}$/i));
+
+my $course_code = $ARGV[0];
+$course_code =~ tr/[a-z]/[A-Z]/;
+$url .= $course_code;
+$url .= "KENS.html";
+
+print "Checking for UNSW course codes with prefix $course_code...\n";
+print "Scanning $url\n";
+
+open F, "wget -q -O- $url|" or die "could not open website";
+
+while (<F>) {
+	next if $_ !~ /href=\"$course_code/;
+	s/<.*.html\">//;
+	s/<\/.*//;
+	s/\s//g;
+	print "$_\n" if $_ =~ /[A-Z]{4}/;
+}
